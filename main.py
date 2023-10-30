13a2e812700f28250a05e025b6843476edf9f8ba
Danila <sedelnikovdanila@gmail.com>
2023-10-30 22:02:20 +0300

    add folders

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphics
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('start_window.ui', self)  # Загружаем дизайн
+        uic.loadUi('ui/start_window.ui', self)  # Загружаем дизайн
         scene = QGraphicsScene(0, 0, 400, 200)
-        scene.addPixmap(QPixmap("logo.jpg"))
+        scene.addPixmap(QPixmap("data/logo.jpg"))
 
         # self.pushButton.clicked.connect(self.run)
         # Обратите внимание: имя элемента такое же как в QTDesigner
@@ -22,7 +22,7 @@ class MyWidget(QMainWindow):
 import hashlib
 
 if __name__ == '__main__':
-    print(hashlib.md5(b'admin_admin').hexdigest())
+    # print(hashlib.md5(b'admin_admin').hexdigest())
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
